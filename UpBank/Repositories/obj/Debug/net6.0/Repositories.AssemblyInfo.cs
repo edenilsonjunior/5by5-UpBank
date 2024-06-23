984c797c873fa087b4428a1c07b3792ae45d4f35
Edenilson Garcia <110670578+edenilsonjunior@users.noreply.github.com>
2024-06-22 23:28:35 -0300

    fix: remover .vs

--- UpBank/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ UpBank/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42976c42ad434f00aec309ad0ce0facda8f1d52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9865a1adb1449102440c178d6be3f6895460ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

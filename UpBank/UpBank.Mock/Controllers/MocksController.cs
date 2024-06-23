a9ef21d6e98f5429bf9a5bdbace21d17d85c3a6d
Edenilson Garcia <110670578+edenilsonjunior@users.noreply.github.com>
2024-06-23 01:03:45 -0300

    feat: api de mock para Account api

---
+++ UpBank/UpBank.Mock/Controllers/MocksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Bank;
+using Models.People;
+using System.Text.Json;
+
+namespace UpBank.Mock.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MocksController : ControllerBase
+{
+    private List<Agency> _agencies;
+    private List<Client> _clients;
+
+    public MocksController()
+    {
+        LoadData();
+    }
+
+    [HttpGet("/GetAgencies")]
+    public List<Agency> GetAgencies() => _agencies;
+
+
+    [HttpGet("/GetClients")]
+    public List<Client> GetClients() => _clients;
+
+
+    private void LoadData()
+    {
+        string agenciesJson = System.IO.File.ReadAllText(@"Mock/agencies.json");
+        string clientsJson = System.IO.File.ReadAllText(@"Mock/clients.json");
+
+        _agencies = JsonSerializer.Deserialize<List<Agency>>(agenciesJson);
+        _clients = JsonSerializer.Deserialize<List<Client>>(clientsJson);
+    }
+
+}
